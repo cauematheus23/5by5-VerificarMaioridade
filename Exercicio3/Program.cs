506ffe3cc3bc21569104917a4d55ecd82f8aa41b
cauematheus23 <cauematheus_23@hotmail.com>
2024-04-18 19:29:23 -0300

    Add project files.

---
+++ Exercicio3/Program.cs
@@ -0,0 +1,18 @@
+﻿int anonasc, idade;
+Console.WriteLine("Digite o ano de seu nascimento: ");
+anonasc = int.Parse(Console.ReadLine());
+if (anonasc > 2024)
+{
+    Console.WriteLine("Ano invalido, digite um ano anterior a 2024");
+}
+else
+{
+    idade = 2024 - anonasc;
+    if (idade >= 18)
+    {
+        Console.WriteLine($"Ja é de maior pois possui {idade} anos");
+    }else
+    {
+        Console.WriteLine($"Ainda não é de maior pois possui {idade} anos");
+            }
+}
